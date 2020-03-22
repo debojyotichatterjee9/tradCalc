b7f2f474cd5bce68636b364c6084d9787440ecc4
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-03-22 19:07:28 +0530

    added new directories

---
+++ src/components/Input.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default class Input extends React.Component<{}> {
+  render() {
+    return (
+      <View>
+        <Text style={{color: '#ffff00', fontSize: 30}}>Dummy Text</Text>
+      </View>
+    );
+  }
+}
